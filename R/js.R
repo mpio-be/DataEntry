98569f06c6800cb24e42aa8c4d77c2579f0fd6d9
valcu <valcu@orn.mpg.de>
2017-05-19 11:57:57 +0200

    js support for keyboard expansion

---
+++ R/js.R
@@ -0,0 +1,24 @@
+# http://stackoverflow.com/questions/5266522/on-keypress-event-how-do-i-change-a-to-a
+# http://keycode.info/
+
+#' @export
+js_insertMySQLTimeStamp <- function() {
+  HTML("
+    <script>
+    $(document).ready(function(event){
+
+       $(document).delegate('input, textarea', 'keyup', function(event){
+
+            date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+            if(event.which === 191) { // 191 forward slash
+                var timestamp = $(this).val().replace('/',date);
+                $(this).val(timestamp);
+            }
+
+        });
+    });
+    </script>
+    ")
+
+}
