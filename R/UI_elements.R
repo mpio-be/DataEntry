02d778791f083e041dd00613f6d4f25b0b5ad7fb
valcu <valcu@orn.mpg.de>
2017-11-16 18:32:09 +0100

    via push all

--- R/UI_elements.R
+++ R/UI_elements.R
@@ -24,25 +24,31 @@ boostrap_table <- function(x, class = 'responsive') {
 #' @param excludeColumns   default 'pk'
 #' @param preFilled        a named list eg. list(datetime_ = as.character(Sys.Date())) 
 #' @export
+#' @examples
+#' emptyFrame(user = 'bt', host = '127.0.0.1', db = 'FIELD_BTatWESTERHOLZ', table = 'ADULTS')
+#' emptyFrame(user = 'bt', host = '127.0.0.1', db = 'FIELD_BTatWESTERHOLZ', table = 'ADULTS', preFilled = list(datetime_ = as.character(Sys.Date())) )
+#' 
 emptyFrame <- function(user, host, db, table,n = 10, excludeColumns = 'pk', preFilled) {
-    F = dbq(user = user, host = host, q = paste0('SELECT * from ', db, '.', table, ' limit 0'), enhance = FALSE )
+    F = dbq(user = user, host = host, q = paste0('SELECT * from ', db, '.', table, ' where FALSE'), enhance = FALSE )
 
     if(!missing(excludeColumns))
     F = F[, setdiff(names(F), excludeColumns), with = FALSE]
+
+
     F = rbind(F, data.table(tempcol = rep(NA, n)), fill = TRUE)[, tempcol := NULL]
 
     if(!missing(preFilled) ) {
         for(i in 1:length(preFilled)) {
             set(F, j = names(preFilled[i]), value = preFilled[[i]])
             }
-    }
+        }
 
-    # convert un-handled rhandsontable types to characters
-    difftime_to_char = which( F[, sapply(.SD, function(x) inherits(x, 'difftime') ) ] ) %>% names
-    F[,(difftime_to_char) := lapply(.SD, as.character), .SDcols = difftime_to_char]
-
-    POSIXt_to_char = which( F[, sapply(.SD, function(x) inherits(x, 'POSIXt') ) ] ) %>% names
-    F[,(POSIXt_to_char) := lapply(.SD, as.character), .SDcols = POSIXt_to_char]
+    # convert un-handled rhandsontable types to characters (RMariaDB)
+      # difftime_to_char = which( F[, sapply(.SD, function(x) inherits(x, 'difftime') ) ] ) %>% names
+      # F[,(difftime_to_char) := lapply(.SD, as.character), .SDcols = difftime_to_char]
+      # 
+      # POSIXt_to_char = which( F[, sapply(.SD, function(x) inherits(x, 'POSIXt') ) ] ) %>% names
+      # F[,(POSIXt_to_char) := lapply(.SD, as.character), .SDcols = POSIXt_to_char]
 
     F
 
